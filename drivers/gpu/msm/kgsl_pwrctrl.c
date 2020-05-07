c205cfcf2a9f24f438c733bfc879cab020547259
celtare21 <celtare21@gmail.com>
2020-05-07 11:43:07 +0200

    gpu: msm: Remove frequency tracing
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -2795,7 +2795,6 @@ static int _wake(struct kgsl_device *device)
 {
 	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
 	int status = 0;
-	unsigned int state = device->state;
 
 	switch (device->state) {
 	case KGSL_STATE_SUSPEND:
