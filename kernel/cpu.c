21331c2fff6686b8227d22ff058694ddf70998b3
Diab Neiroukh <lazerl0rd@thezest.dev>
2021-01-29 09:38:49 +0000

    HACK: cpumask: Add cpu_hp_mask which contains both the big and prime clusters.
    This is needed to sanely balance IRQs on devices with only one CPU in
    each cluter.
    
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: jameskunst <jamesandrews@outlook.com.br>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2394,6 +2394,17 @@ const struct cpumask *const cpu_prime_mask = cpu_possible_mask;
 #endif
 EXPORT_SYMBOL(cpu_prime_mask);
 
+#if defined(CONFIG_BIG_CPU_MASK) && defined(CONFIG_PRIME_CPU_MASK)
+static const unsigned long hp_cpu_bits = CONFIG_BIG_CPU_MASK + CONFIG_PRIME_CPU_MASK;
+const struct cpumask *const cpu_hp_mask = to_cpumask(&hp_cpu_bits);
+#else
+const struct cpumask *const cpu_hp_mask = cpu_possible_mask;
+#endif
+EXPORT_SYMBOL(cpu_hp_mask);
+
+unsigned int __cpu_psci_id[NR_CPUS];
+EXPORT_SYMBOL(__cpu_psci_id);
+
 void init_cpu_present(const struct cpumask *src)
 {
 	cpumask_copy(&__cpu_present_mask, src);
