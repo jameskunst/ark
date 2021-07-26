lzlrd/tryme_redbull

HACK: kernel: Add a mixed big and prime cluster mode to the perf-critical API.
Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>

include/linux/kthread.h
@@ -61,7 +61,8 @@ struct task_struct *kthread_create_on_cpu(int (*threadfn)(void *data),
 	if (!IS_ERR(__k)) {						   \
 		__k->flags |= PF_PERF_CRITICAL;				   \
 		BUILD_BUG_ON(perfmask != cpu_perf_mask &&		   \
-			     perfmask != cpu_perfp_mask);		   \
+			     perfmask != cpu_perfp_mask &&		   \
+			     perfmask != cpu_hp_mask);			   \
 		kthread_bind_mask(__k, perfmask);			   \
 		wake_up_process(__k);					   \
 	}								   \

kernel/irq/manage.c
@@ -34,6 +34,7 @@ static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
 static int perfp_cpu_index = -1;
+static int hp_cpu_index = -1;
 static bool perf_crit_suspended;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
@@ -1225,8 +1226,10 @@ static void affine_one_perf_thread(struct irqaction *action)
 
 	if (action->flags & IRQF_PERF_AFFINE)
 		mask = cpu_perf_mask;
-	else
+	else if (action->flags & IRQF_PERFP_AFFINE)
 		mask = cpu_perfp_mask;
+	else
+		mask = cpu_hp_mask;
 
 	action->thread->flags |= PF_PERF_CRITICAL;
 	set_cpus_allowed_ptr(action->thread, mask);
@@ -1250,9 +1253,12 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	if (perf_flag & IRQF_PERF_AFFINE) {
 		mask = cpu_perf_mask;
 		mask_index = &perf_cpu_index;
-	} else {
+	} else if (perf_flag & IRQF_PERFP_AFFINE) {
 		mask = cpu_perfp_mask;
 		mask_index = &perfp_cpu_index;
+	} else {
+		mask = cpu_hp_mask;
+		mask_index = &hp_cpu_index;
 	}
 
 	if (!cpumask_intersects(mask, cpu_online_mask)) {
@@ -1615,7 +1621,8 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PERFP_AFFINE)) {
+		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PERFP_AFFINE |
+				  IRQF_HP_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;

kernel/sched/core.c
@@ -1687,7 +1687,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 
 	/* Don't allow perf-critical threads to have non-perf affinities */
 	if ((p->flags & PF_PERF_CRITICAL) && new_mask != cpu_perf_mask &&
-	    new_mask != cpu_perfp_mask)
+	    new_mask != cpu_perfp_mask && new_mask != cpu_hp_mask)
 		return -EINVAL;
 
 	rq = task_rq_lock(p, &rf);

jameskunst/ark

Signed-off-by: jameskunst <jamesandrews@outlook.com.br>

include/linux/kthread.h
@@ -64,7 +64,8 @@ bool kthread_is_per_cpu(struct task_struct *k);
 	if (!IS_ERR(__k)) {						   \
 		__k->flags |= PF_PERF_CRITICAL;				   \
 		BUILD_BUG_ON(perfmask != cpu_perf_mask &&		   \
-			     perfmask != cpu_prime_mask);		   \
+			     perfmask != cpu_prime_mask &&		   \
+			     perfmask != cpu_hp_mask);			\
 		kthread_bind_mask(__k, perfmask);			   \
 		wake_up_process(__k);					   \
 	}								   \

kernel/irq/manage.c
@@ -34,6 +34,7 @@ static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
 static int prime_cpu_index = -1;
+static int hp_cpu_index = -1;
 static bool perf_crit_suspended;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
@@ -1200,8 +1201,10 @@ static void affine_one_perf_thread(struct irqaction *action)
 
 	if (action->flags & IRQF_PERF_AFFINE)
 		mask = cpu_perf_mask;
-	else
+	else if (action->flags & IRQF_PERFP_AFFINE)
 		mask = cpu_prime_mask;
+	else
+		mask = cpu_hp_mask;
 
 	action->thread->flags |= PF_PERF_CRITICAL;
 	set_cpus_allowed_ptr(action->thread, mask);
@@ -1225,9 +1228,12 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	if (perf_flag & IRQF_PERF_AFFINE) {
 		mask = cpu_perf_mask;
 		mask_index = &perf_cpu_index;
-	} else {
+	} else if (perf_flag & IRQF_PRIME_AFFINE) {
 		mask = cpu_prime_mask;
 		mask_index = &prime_cpu_index;
+	} else {
+		mask = cpu_hp_mask;
+		mask_index = &hp_cpu_index;
 	}
 
 	if (!cpumask_intersects(mask, cpu_online_mask)) {
@@ -1573,7 +1579,8 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE)) {
+		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE |
+				  IRQF_HP_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;

kernel/sched/core.c
@@ -1119,7 +1119,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 
 	/* Don't allow perf-critical threads to have non-perf affinities */
 	if ((p->flags & PF_PERF_CRITICAL) && new_mask != cpu_perf_mask &&
-	    new_mask != cpu_prime_mask)
+	    new_mask != cpu_prime_mask && new_mask != cpu_hp_mask)
 		return -EINVAL;
 
 	rq = task_rq_lock(p, &rf);
bee88e828e9d90928a5ce0caae76f34361c16d2d
Diab Neiroukh <lazerl0rd@thezest.dev>
2021-01-29 10:03:16 +0000

    HACK: kernel: Add a mixed big and prime cluster mode to the perf-critical API.
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: jameskunst <jamesandrews@outlook.com.br>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -34,6 +34,7 @@ static LIST_HEAD(perf_crit_irqs);
 static DEFINE_RAW_SPINLOCK(perf_irqs_lock);
 static int perf_cpu_index = -1;
 static int prime_cpu_index = -1;
+static int hp_cpu_index = -1;
 static bool perf_crit_suspended;
 
 #ifdef CONFIG_IRQ_FORCED_THREADING
@@ -1200,8 +1201,10 @@ static void affine_one_perf_thread(struct irqaction *action)
 
 	if (action->flags & IRQF_PERF_AFFINE)
 		mask = cpu_perf_mask;
-	else
+	else if (action->flags & IRQF_PERFP_AFFINE)
 		mask = cpu_prime_mask;
+	else
+		mask = cpu_hp_mask;
 
 	action->thread->flags |= PF_PERF_CRITICAL;
 	set_cpus_allowed_ptr(action->thread, mask);
@@ -1225,9 +1228,12 @@ static void affine_one_perf_irq(struct irq_desc *desc, unsigned int perf_flag)
 	if (perf_flag & IRQF_PERF_AFFINE) {
 		mask = cpu_perf_mask;
 		mask_index = &perf_cpu_index;
-	} else {
+	} else if (perf_flag & IRQF_PRIME_AFFINE) {
 		mask = cpu_prime_mask;
 		mask_index = &prime_cpu_index;
+	} else {
+		mask = cpu_hp_mask;
+		mask_index = &hp_cpu_index;
 	}
 
 	if (!cpumask_intersects(mask, cpu_online_mask)) {
@@ -1573,7 +1579,8 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			irqd_set(&desc->irq_data, IRQD_NO_BALANCING);
 		}
 
-		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE)) {
+		if (new->flags & (IRQF_PERF_AFFINE | IRQF_PRIME_AFFINE |
+				  IRQF_HP_AFFINE)) {
 			affine_one_perf_thread(new);
 			irqd_set(&desc->irq_data, IRQD_PERF_CRITICAL);
 			*old_ptr = new;
