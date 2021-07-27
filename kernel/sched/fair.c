8f689ab3519daac45e477833942ead412a899f7e
James Kunst Andrews <jamesandrews@outlook.com.br>
2021-07-27 12:13:10 -0300

    sched: fair: remove schedtune's influence over frequencies
    This will keep biasing tasks to higher-capacity CPUs without having any influence over frequency selection.
    
    Signed-off-by: James Kunst Andrews <jamesandrews@outlook.com.br>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6757,7 +6757,7 @@ boosted_cpu_util(int cpu, struct sched_walt_cpu_load *walt_load)
 
 	trace_sched_boost_cpu(cpu, util, margin);
 
-	return util + margin;
+	return util;
 }
 
 static inline unsigned long
@@ -6768,7 +6768,7 @@ boosted_task_util(struct task_struct *task)
 
 	trace_sched_boost_task(task, util, margin);
 
-	return util + margin;
+	return util;
 }
 
 static unsigned long cpu_util_without(int cpu, struct task_struct *p);
