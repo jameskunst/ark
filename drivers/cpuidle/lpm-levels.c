b64c88b1376a030bcfdcc08f95cb869cec2bce2f
Sultan Alsawaf <sultan@kerneltoast.com>
2021-08-23 00:47:31 -0700

    cpuidle: lpm-levels: Only cancel the bias timer when it's used
    The bias timer is only started when WFI is used, so we only need to
    try and cancel it after leaving WFI.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1367,7 +1367,8 @@ exit:
 	dev->last_residency = ktime_us_delta(ktime_get(), start);
 	update_history(dev, idx);
 	if (cpu->bias) {
-		biastimer_cancel();
+                if (!idx)
+			biastimer_cancel();
 		cpu->bias = 0;
 	}
 	local_irq_enable();
